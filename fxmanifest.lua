6742f53643fd78f8bfd23ff447d77a5b2431b13f
NoobySloth <noobytubez@gmail.com>
2022-06-21 13:08:51 +0100

    fxmanifest redo

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,17 +2,13 @@ fx_version 'cerulean'
 games {'gta5'}
 
 files{
-	'meta/**/*weaponcomponents.meta',
-	'meta/**/*weaponarchetypes.meta',
-	'meta/**/*weaponanimations.meta',
-	'meta/**/*pedpersonality.meta',
-	'meta/**/*weapons.meta',
+	'metas/**/*.meta'
 }
 
-data_file 'WEAPONCOMPONENTSINFO_FILE' 'meta/**/*weaponcomponents.meta'
-data_file 'WEAPON_METADATA_FILE' 'meta/**/*weaponarchetypes.meta'
-data_file 'WEAPON_ANIMATIONS_FILE' 'meta/**/*weaponanimations.meta'
-data_file 'PED_PERSONALITY_FILE' 'meta/**/*pedpersonality.meta'
-data_file 'WEAPONINFO_FILE' 'meta/**/*weapons.meta'
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'metas/**/weaponcomponents.meta'
+data_file 'WEAPON_METADATA_FILE' 'metas/**/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'metas/**/weaponanimations.meta'
+data_file 'PED_PERSONALITY_FILE' 'metas/**/pedpersonality.meta'
+data_file 'WEAPONINFO_FILE' 'metas/**/weapons.meta'
 
 client_script 'cl_weaponNames.lua'
